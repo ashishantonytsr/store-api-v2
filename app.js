52f440c1e3a3c1b6c4a37e52cbdf4706f8457db2
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-08 11:28:07 +0530

    created index route, connected to db

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// imports
+const connectDB = require('./db/connect')
+
+// routes
+app.get('/', (req,res)=>{
+	res.send('store-api-v2')
+})
+
+const port = process.env.PORT
+
+const start = async()=>{
+	try {
+		console.log('Connecting to Database...');
+		await connectDB(process.env.MONGO_URI)
+		await app.listen(port, ()=> console.log(`Server is listening on port ${port}`) )
+	} catch (error) {
+		console.log(error);		
+	}
+}
+
+start()
