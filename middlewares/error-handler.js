37fd99d3af666cf266010dd14294a153fb1179a8
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-08 11:55:46 +0530

    added middleware for error handling

---
+++ middlewares/error-handler.js
@@ -0,0 +1,10 @@
+const {StatusCodes} = require('http-status-codes')
+const CustomAPIError = require('../errors')
+
+const errorHandlerMiddleware = async (err, req, res, next)=>{
+	let errorObj = {
+		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+		msg: err.message || 'Something went wrong.. Try later...'
+	}
+	return res.status(errorObj.statusCode).json({ success: false, msg: errorObj.msg })
+}
