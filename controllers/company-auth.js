0b3dc8d6871854282481462aa8b60c359e012aea
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-08 17:02:27 +0530

    initial test: auth route to company

--- controllers/company-auth.js
+++ controllers/company-auth.js
@@ -1,5 +1,24 @@
+const Company = require('../models/Company')
+
 const register = async (req, res) => {
-  res.json({ token: 'nothing' })
+  const { name, email, password, contact_number } = req.body
+  try {
+    const company = await Company.create({
+      name,
+      email,
+      password,
+      contact_number,
+    })
+    res
+      .status(200)
+      .json({
+        success: true,
+        msg: `Added company ${company.name}`,
+        data: company,
+      })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const login = async (req, res) => {
