2fae383f8af3b784d8794926a393e7351983f822
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-08 16:37:41 +0530

    minor changes

--- errors/index.js
+++ errors/index.js
@@ -4,8 +4,8 @@ const UnauthenticatedError = require('./unauthenticated')
 const NotFoundError = require('./not-found')
 
 module.exports = {
-	CustomAPIError,
-	BadRequestError,
-	UnauthenticatedError,
-	NotFoundError
+  CustomAPIError,
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError,
 }
