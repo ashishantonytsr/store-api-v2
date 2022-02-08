184db900ee0935ac7c3290cbabf7cf1f710ca569
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-08 11:28:29 +0530

    added model company

---
+++ models/Company.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const CompanySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please provide company name'],
+		minlength: 3,
+		maxlength: 20
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide email'],
+		minlength: 3,
+		match: ['/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', 'Please provide valid email'],
+		unique: true
+	},
+	password: {
+		type: String,
+		required: [true, 'Please provide password'],
+		minlength: 8
+	},
+	address: {
+		type: String,
+		minlength: 15
+	},
+	contact_number: {
+		type: Number,
+		required: [true, 'Please provide contact number'],
+		minlength: 5
+	}
+})
+
+// to hash password before storing
+CompanySchema.pre('save', async function (next){
+	const salt = await bcrypt.genSalt(10)
+	this.password = await bcrypt.hash(this.password, salt)
+	next()
+})
+
+// to compare password
+CompanySchema.methods.comparePassword = async function(candidatePassword){
+	const isMatch = await bcrypt.compare(candidatePassword, this.password)
+	return isMatch
+}
+
+module.exports = mongoose.model('Company', CompanySchema)
