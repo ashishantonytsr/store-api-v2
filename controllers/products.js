8d6874c6ef6cb724213b76f566fa80e648674057
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-10 12:52:08 +0530

    added controller for create product route

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,8 @@
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const ProductModel = require('../models/Product')
+const CompanyModel = require('../models/Company')
+
 const getAllProducts = async (req, res) => {
   res.send('get all products')
 }
@@ -10,8 +15,29 @@ const getProductReviews = async (req, res) => {
   res.send('reviews on product x')
 }
 
+// company only
 const createProduct = async (req, res) => {
-  res.send('create products')
+  // duplicate validation
+  const isDuplicate = await ProductModel.find({
+    company_id: req.user.companyId,
+  })
+    .where('title')
+    .equals(req.body.title)
+    .where('price')
+    .equals(req.body.price)
+
+  if (isDuplicate && isDuplicate != '') {
+    throw new BadRequestError('Duplicates found')
+  }
+
+  const product = await ProductModel.create(req.body)
+  // 626b, 5.15s // 603b, 1.2s
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    msg: `Product '${product.title}' successfully added`,
+    user: { name: req.user.name, email: req.user.email },
+    data: product,
+  })
 }
 
 const updateProduct = async (req, res) => {
