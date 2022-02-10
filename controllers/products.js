7e33be407875e7b67e0414a6d8b71aae67fe5d38
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-10 20:40:13 +0530

    added dynamic routes
    added route for single product fetch & made get requests dynamic

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,69 @@
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError,
+} = require('../errors')
 const ProductModel = require('../models/Product')
 const CompanyModel = require('../models/Company')
 
 const getAllProducts = async (req, res) => {
-  const product = await ProductModel.find({
-    'company_id': { $eq: req.user.companyId },
-  })
-  res.json({
+  let products = {}
+
+  // init queryObj
+  // collect data from req.query
+  // validata data & add to queryObj
+  // if numericFilter: create operatormap
+  // replace <><=>= with $lt, $gt, $lte, $gte in numericFilters
+
+  if (req.user.companyId) {
+    products = await ProductModel.find({
+      'company_id': { $eq: req.user.companyId },
+    }) // add queryObj
+  }
+
+  // if (req.user.userId) {
+  //   products = await ProductModel.find({
+  //     // queryObj only
+  //   })
+  // }
+
+  // if sort is provided as query params; sort products
+  // if fields are provided as query params, select fields from product
+  // if page/limit is provided,
+  // create skip as (page -1)* limit
+  // products.limit().skip()
+
+  res.status(StatusCodes.OK).json({
     success: true,
-    count: product.length,
-    product: product,
+    count: products.length,
+    product: products,
   })
 }
 
 const getSingleProduct = async (req, res) => {
-  res.send('get single product')
+  let product = {}
+
+  // if logged in as company
+  if (req.user.companyId) {
+    product = await ProductModel.find({
+      '_id': { $eq: req.params.id },
+      'company_id': { $eq: req.user.companyId },
+    })
+  }
+
+  // // if logged in as customer
+  // if (req.user.userId) {
+  //   product = await ProductModel.find({
+  //     '_id': { $eq: req.params.id },
+  //   })
+  // }
+
+  if (!product || product == '') {
+    throw new BadRequestError(`No product with id '${req.params.id}'`)
+  }
+
+  res.status(StatusCodes.OK).json({ product })
 }
 
 const getProductReviews = async (req, res) => {
@@ -23,6 +71,12 @@ const getProductReviews = async (req, res) => {
 }
 
 const createProduct = async (req, res) => {
+  // TODO: check if customer can create product
+
+  // if (req.user.userId){
+  // 	throw new NotFoundError('Route does not exist')
+  // }
+
   // duplicate validation
   const isDuplicate = await ProductModel.find({
     'company_id': { $eq: req.user.companyId },
@@ -38,12 +92,12 @@ const createProduct = async (req, res) => {
   res.status(StatusCodes.CREATED).json({
     success: true,
     msg: `Product '${product.title}' successfully added`,
-    // user: { name: req.user.name, email: req.user.email },
     data: product,
   })
 }
 
 const updateProduct = async (req, res) => {
+  // check if there is a company with companyId
   res.send('update products')
 }
 
