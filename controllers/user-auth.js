4a1e19f60a3d9b7fbece791375a975e47f6c0d05
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-11 12:36:49 +0530

    added user register route

--- controllers/user-auth.js
+++ controllers/user-auth.js
@@ -1,5 +1,16 @@
+const { StatusCodes } = require('http-status-codes')
+const UserModel = require('../models/User')
+
 const register = async (req, res) => {
-  res.send('customer register route')
+  // Validation is done by mongoose at the time of insertion
+  const user = await UserModel.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    msg: `User successfully registered`,
+    user: { name: user.name, email: user.email },
+    token: token,
+  })
 }
 
 const login = async (req, res) => {
