48aeda1e67c751acf42c8faa7af00d084dd5f0c0
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-12 20:06:13 +0530

    added comments

--- routes/company-auth.js
+++ routes/company-auth.js
@@ -1,3 +1,5 @@
+// TODO: use one file for authentication; both from user side & company side
+
 const express = require('express')
 const router = express.Router()
 const { register, login } = require('../controllers/company-auth')
