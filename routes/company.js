acd1ea88ccd469643e4797c24dd66eaf94b7ea76
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-13 08:48:43 +0530

    renamed to company.js

---
+++ routes/company.js
@@ -0,0 +1,24 @@
+// TODO: use one file for authentication; both from user side & company side
+
+const express = require('express')
+const router = express.Router()
+const {
+  register,
+  login,
+  getProfile,
+  updateProfile,
+  deleteProfile,
+} = require('../controllers/company')
+
+const { authMiddleware } = require('../middlewares/company-auth')
+
+router.post('/auth/register', register)
+router.post('/auth/login', login)
+
+router
+  .route('/:id')
+  .get(authMiddleware, getProfile)
+  .patch(authMiddleware, updateProfile)
+  .delete(authMiddleware, deleteProfile)
+
+module.exports = router
